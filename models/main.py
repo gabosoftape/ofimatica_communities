96745045b8d6ec97ad1885d39d46a3352eef02cf
gabriel <gabosoft.ape@gmail.com>
2019-10-08 12:12:47 -0500

    update comunities models

--- models/main.py
+++ models/main.py
@@ -1,8 +1,2 @@
 from odoo import models, fields, api, _
 from datetime import datetime
-
-class recibosAdmin(models.Model):
-    _name='property.liabilities'
-    _inherit='account.invoice'
-
-    tipo = fields.Char('Tipo')
